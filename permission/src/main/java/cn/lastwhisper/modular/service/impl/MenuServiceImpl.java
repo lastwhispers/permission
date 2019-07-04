af95e6672db1a74ca059c98a0d9bed2b9033449c
gggb2312 <ggb2312@gmail.com>
2019-07-04 12:55:07 +0800

    更新

--- permission/src/main/java/cn/lastwhisper/modular/service/impl/MenuServiceImpl.java
+++ permission/src/main/java/cn/lastwhisper/modular/service/impl/MenuServiceImpl.java
@@ -13,6 +13,8 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
@@ -38,24 +40,28 @@ import redis.clients.jedis.Jedis;
 @Transactional(isolation = Isolation.REPEATABLE_READ, propagation = Propagation.REQUIRED)
 @Service
 public class MenuServiceImpl implements MenuService {
+	
+	private static Logger logger = LoggerFactory.getLogger(MenuServiceImpl.class);
+	
 	@Autowired
 	private MenuMapper menuMapper;
 
 	@Autowired
 	private Jedis jedis;
-	
-	@Transactional(propagation =Propagation.NOT_SUPPORTED)
+
+	@Transactional(propagation = Propagation.NOT_SUPPORTED)
 	@Override
 	public List<Tree> findMenuList() {
 		List<Tree> tree = menuMapper.selectMenuList();
 		return tree;
 	}
-	
-	@Transactional(propagation =Propagation.NOT_SUPPORTED)
+
+	@Transactional(propagation = Propagation.NOT_SUPPORTED)
 	@Override
 	public List<Menu> findMenuById(String menuid) {
 		return menuMapper.selectMenuById(menuid);
 	}
+
 	@LogAnno(operateType = "添加权限菜单")
 	@Override
 	public GlobalResult addMenu(Menu menu) {
@@ -159,8 +165,9 @@ public class MenuServiceImpl implements MenuService {
 		if (easyuiMenusJson == null) {
 			// 获取根菜单
 			List<Menu> root = menuMapper.selectMenu("-1");
-			// 用户下的菜单集合
-//			List<Menu> userMenus = menuMapper.selectMenusByUserid(userid);
+			// 用户下的菜单集合 找数据库
+//			List<Menu> userMenus = menuMapper.selectMenuByUserid(userid);
+			// 用户下的菜单集合 找缓存
 			List<Menu> userMenus = findMenuListByUserid(userid);
 			// 根菜单
 			menu = cloneMenu(root.get(0));
@@ -191,12 +198,14 @@ public class MenuServiceImpl implements MenuService {
 					menu.getMenus().add(_m1);
 				}
 			}
-			System.out.println("从数据库读取，设置缓存");
+			logger.debug("从数据库读取，设置缓存");
+//			System.out.println("从数据库读取，设置缓存");
 			jedis.set("menusEasyui_" + userid, JSON.toJSONString(menu));
 		} else {
 			menu = JSON.parseObject(easyuiMenusJson, Menu.class);
 //			menu = JSON.parseArray(easyuiMenusJson, Menu.class).get(0);
-			System.out.println("从缓存读取");
+//			System.out.println("从缓存读取");
+			logger.debug("从缓存读取");
 		}
 		return menu;
 	}
@@ -209,11 +218,13 @@ public class MenuServiceImpl implements MenuService {
 			// 1.从数据库中查出来，放入缓存中
 			menuList = menuMapper.selectMenuByUserid(userid);
 			jedis.set("menusList_" + userid, JSON.toJSONString(menuList));
-			System.out.println("从数据库中查询menuList");
+			logger.debug("从数据库中查询menuList");
+//			System.out.println("从数据库中查询menuList");
 		} else {
 			// 2.直接从缓存中拿
+			logger.debug("从缓存中查询menuList"+menuListJson);
 			menuList = JSON.parseArray(menuListJson, Menu.class);
-			System.out.println("从缓存中查询menuList");
+//			System.out.println("从缓存中查询menuList");
 		}
 		return menuList;
 	}
